77ad88b68a3fda4fa53c752bffd171920d95a1fb
longquan98@gmail.com <longquan98@gmail.com>
2024-04-05 01:16:58 -0400

    First commit

---
+++ NugetLibrary/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UtilityLibraries;
+
+Console.WriteLine(StringLibrary.StartsWithUpper(null));
+Console.WriteLine(StringLibrary.StartsWithUpper("Hello World"));
